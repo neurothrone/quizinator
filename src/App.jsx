77c86f010fc7c4f6878364fd92f4a5ab1382674f
Zane Neurothrone <54468049+neurothrone@users.noreply.github.com>
2025-01-19 21:21:10 +0100

    implement part of game functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,16 @@ import './App.css'
 // import Header from "./components/Layout/Header.jsx";
 // import Footer from "./components/Layout/Footer.jsx";
 // import HomePage from "./components/Pages/HomePage.jsx";
-// import GamePage from "./components/Pages/GamePage.jsx";
-import ResultPage from "./components/Pages/ResultPage.jsx";
+import GamePage from "./components/Pages/GamePage.jsx";
+// import ResultPage from "./components/Pages/ResultPage.jsx";
 
 function App() {
   return (
     <>
       {/*<Header />*/}
       {/*<HomePage/>*/}
-      {/*<GamePage/>*/}
-      <ResultPage/>
+      <GamePage/>
+      {/*<ResultPage/>*/}
       {/*<Footer />*/}
     </>
   )
--- src/components/Pages/GamePage.jsx
+++ src/components/Pages/GamePage.jsx
@@ -1,11 +1,51 @@
-﻿const GamePage = () => {
+﻿import {useEffect, useState} from "react";
+
+const GamePage = () => {
+  const [questions, setQuestions] = useState([]);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [score, setScore] = useState(0);
+
+  useEffect(() => {
+    fetch("/data.json")
+      .then(response => response.json())
+      .then(data => setQuestions(data))
+      .catch(error => console.error("Error loading data:", error));
+  }, []);
+
+  function onAnswerClicked(answer) {
+    const question = questions[currentQuestion];
+    if (answer === question.answer) {
+      // TODO: Show the correct feedback
+      alert("Correct!");
+      setScore(score + 1);
+    } else {
+      // TODO: Show the incorrect feedback
+      alert("Incorrect!");
+    }
+
+    if (currentQuestion < questions.length - 1) {
+      setCurrentQuestion(currentQuestion + 1);
+    } else {
+      // TODO: Show the result page
+      alert(`Game Over! Your score is ${score}`);
+    }
+  }
   return (
     <main>
-      <h2>What is 2 + 2?</h2>
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>4</button>
+      {questions.length === 0
+        ? <p>Loading...</p>
+        : <div>
+          <h2>{questions[currentQuestion].question}</h2>
+          <ul>
+            {questions[currentQuestion].options.map((answer, index) => (
+              <li key={index}>
+                <button onClick={() => onAnswerClicked(answer)}>
+                  {answer}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>}
     </main>
   );
 };
