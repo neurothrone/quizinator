452242b9142e1787e098851288f595a7b509ecff
Zane Neurothrone <54468049+neurothrone@users.noreply.github.com>
2025-01-19 20:29:25 +0100

    add layout components and pages

---
+++ src/components/Layout/Footer.jsx
@@ -0,0 +1,7 @@
+﻿const Footer = () => {
+  return (
+    <footer>Copyright &copy; 20252 Quizinator Corp. All rights reserved.</footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Pages/GamePage.jsx
@@ -0,0 +1,13 @@
+﻿const GamePage = () => {
+  return (
+    <main>
+      <h2>What is 2 + 2?</h2>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>4</button>
+    </main>
+  );
+};
+
+export default GamePage;
